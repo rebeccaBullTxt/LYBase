d435ef9f7272825241302ef7e6b695e4deb1053e
rebeccabull <rebeccaBull@sina.com>
2019-08-01 09:12:39 +0800

    Project initialization

---
+++ BaseFrame/LYFrameDemo/LYFrameDemo/Main/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYFrameDemo
+//
+//  Created by 刘渊 on 2019/8/1.
+//  Copyright © 2019 刘渊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
